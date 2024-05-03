8b35f496863b72d910a33881813b863fbc388dc5
Guilherme <g.satilio@gmail.com>
2024-05-03 11:52:33 -0300

    Add project files.

---
+++ Trab_PessoaIdadeBissexto/Pessoa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trab_PessoaIdadeBissexto
+{
+    internal class Pessoa
+    {
+        string nome;
+        int idade;
+        string profissao;
+
+        public Pessoa(string nome, int idade, string profissao)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.profissao = profissao;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        public void setProfissao(string profissao)
+        {
+            this.profissao = profissao;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public int getIdade()
+        {
+            return this.idade;
+        }
+        public string getProfissao()
+        {
+            return this.profissao;
+        }
+    }
+}
---
+++ Trab_PessoaIdadeBissexto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Trab_PessoaIdadeBissexto;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa p1 = criarPessoa();
+        Pessoa p2 = criarPessoa();
+        Pessoa p3 = criarPessoa();
+
+        retornarDados(p1);
+        retornarDados(p2);
+        retornarDados(p3);
+    }
+    static Pessoa criarPessoa()
+    {
+        Pessoa temp;
+        string nome, profissao;
+        int idade;
+        Console.WriteLine("Informe o nome da pessoa");
+        nome = Console.ReadLine();
+        Console.WriteLine("Informe a idade da pessoa");
+        idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Informe a profissao da pessoa");
+        profissao = Console.ReadLine();
+        temp = new Pessoa(nome, idade, profissao);
+
+        return temp;
+    }
+    static void retornarDados(Pessoa p)
+    {
+        int idade = 0, ano = 0, idadeaux = 0, anonascimento = 0;
+        Console.WriteLine("Nome: " + p.getNome());
+        Console.WriteLine("Idade: " + p.getIdade());
+        Console.WriteLine("Profissão: " + p.getProfissao());
+        idade = p.getIdade();
+        anonascimento = (DateTime.Now.Year - idade);
+
+        for(int i = 0; i <= idade; i++)
+        {
+            if(DateTime.DaysInMonth(anonascimento, 02) == 29)
+            {
+                idadeaux++;
+            }
+            anonascimento++;
+        }
+        Console.WriteLine("Idade em Anos Bissextos: " + idadeaux);
+    }
+}
